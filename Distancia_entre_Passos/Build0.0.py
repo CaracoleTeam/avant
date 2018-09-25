3f3ef137591c8112613b8f9a9dcbcfbb90d154f1
Victor Almeida <vala@cesar.school>
2018-09-25 14:48:02 -0300

    Update

---
+++ Distancia_entre_Passos/Build0.0.py
@@ -0,0 +1,6 @@
+from math import *
+
+s0 = float(input("Digite a posição 1: "))
+s1 = float(input("Digite a posição 2: "))
+sf = s0 - s1
+print("A distancia entre o ponto {} e o ponto {} é {}".format(s0,s1,sf))
